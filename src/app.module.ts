c57739842c7ca29dbbb622a52faaf641c64ce2a3
Frank Mendez <frankmendezresources@gmail.com>
2021-03-11 02:14:55 +0800

    Setting up the JWT Strategy for Authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    TasksModule,
+    AuthModule,
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
